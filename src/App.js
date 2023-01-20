1780bdac9ee7f2008041607eae69de9cacf03d59
LucasAstino <116663021+LucasAstino@users.noreply.github.com>
2023-01-20 16:26:49 -0300

    adicionando arquivos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
+import React, { Component } from "react";
 import "./styles.css";
+import Table from "./components/tableany/tableany";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+export default class BASE extends React.Component {
+  constructor(props) {
+    super(props); //quando for usar props
+    this.state = {
+      login: true,
+      Darktheme: true
+    };
+  }
+
+  componentDidMount() {
+    if (document.querySelector("body").classList == "active") {
+      this.setState({ Darktheme: !this.state.Darktheme });
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Table />
+      </>
+    );
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
-import { StrictMode } from "react";
+import React from "react";
 import { createRoot } from "react-dom/client";
 
+import Header from "./components/header/Header";
 import App from "./App";
+import SubHeader from "./subHeader";
+import Cadastro from "./components/Login/cadastro";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+function thictoc() {
+  root.render(
+    <>
+      <Header nav1="home" nav2="contact" nav3="info" nav4="about" />
+      <SubHeader />
+      <Cadastro />
+      <App />
+    </>
+  );
+}
+
+setInterval(thictoc, 1000);
